7ff837ad8d87ba5d6ddaefeff65b80f95f1d0279
Patrick Grasseels <grasseelsp@hotmail.com>
2019-10-20 21:21:13 +0200

    Clean

--- src/BoardApp/Program.cs
+++ src/BoardApp/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace TodoApp
+namespace BoardApp
 {
     public class Program
     {
--- src/BoardApp/Startup.cs
+++ src/BoardApp/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace TodoApp
+namespace BoardApp
 {
     public class Startup
     {
